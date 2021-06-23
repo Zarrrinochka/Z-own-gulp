4769ae147409e0823dd9713ff64810f2c375b4b0
Zarina <1005534@bk.ru>
2021-06-23 18:09:45 +0700

    gulp pug sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,46 @@
-const { src, dest, watch, parallel, series } = require("gulp");
-const autoprefixer = require("gulp-autoprefixer");
-const browserSync = require("browser-sync").create();
-const scss = require("gulp-sass");
+"use strict" /*по умолчанию изменения ECMAScript 5 не применяются, нужно явно их активировать с помощью директивы: "use strict".*/
+
+var gulp = require("gulp"),
+    pug = require("gulp-pug"),
+    scss = require("gulp-sass"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browserSync = require("browser-sync").create(),
+    autoprefixer = require("gulp-autoprefixer");
 
 
+gulp.task("pug", function () {
+    return gulp.src("src/pug/pages/*.pug")
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest("build"))
+});
 
-function styles() {
-    return src("#src/sass/style.scss") /* найти путь к файлу с которым нужно работать */
-        .pipe(scss()) /*что с ним сделать - чз gulp-sass преврати scss в css*/
+gulp.task("styles", function () {
+    return gulp.src("src/static/sass/style.scss")
+        .pipe(sourcemaps.init())
+        .pipe(scss())
         .pipe(autoprefixer({
             overrideBrowserslist: ["last 10 version"],
             grid: true
         }))
-        .pipe(dest("dist/css"))  /*что с ним сделать - destнуть в указанную папку*/
-        .pipe(browserSync.stream()) /*что с ним сделать - перезапустить браузер*/
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest("build"))
+});
+
+gulp.task("watch", function () {
+    gulp.watch("src/pug/pages/**/*.pug", gulp.series("pug")),
+    gulp.watch("src/static/sass/**/*.scss", gulp.series("styles"))
 }
-exports.styles = styles;  /*завершение, чтоб заработало*/
+)
 
-
-function html() {
-    return src("#src/*.html")
-        .pipe(dest("dist"))
-        .pipe(browserSync.stream()) 
-}
-exports.html = html;
-
-
-// Определяем логику работы Browsersync
-function browsersync() {
+gulp.task("serve", function () {
     browserSync.init({ // Инициализация Browsersync
-        server: { baseDir: "dist/" }, // Указываем папку сервера
-        notify: false, // Отключаем уведомления
-        online: true // Режим работы: true или false
+        server: { baseDir: "./build" }, // Указываем папку сервера
     })
-}
-// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
-exports.browsersync = browsersync;
+    browserSync.watch("build", browserSync.reload);
+});
 
-
-function watcher() {
-    watch(["#src/**/*.scss"], styles); /*следи за фаилами по этому пути, при из изменении запускай styles*/
-    watch(["#src/*.html"], html).on('change', browserSync.reload);
-    //watch(["#src/js/script.js"], scripts);
-}
-exports.watcher = watcher;
-
-// Copy
-
-function copy() {
-    src([
-        "#src/img/**/*.{png,jpg,svg}",
-        "#src/fonts/*.{woff2,woff}",
-        "#src/*.ico",
-        "#src/img/**/*.svg"
-    ], {
-        base: "#src"
-    })
-        .pipe(dest("dist"))
-}
-
-exports.copy = copy;
-
-
-
-
-exports.default = parallel(html, copy, styles, watcher, browsersync);
+gulp.task("default", gulp.series(
+    gulp.parallel("pug", "styles"), gulp.parallel("watch", "serve")
+));
